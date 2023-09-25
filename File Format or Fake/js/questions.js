4785658d7d1d2b1ea20c1b2cc81a0ac42bad4729
Karyn <51198063+KarynW13@users.noreply.github.com>
2023-09-25 16:50:51 +0100

    Add files via upload

---
+++ File Format or Fake/js/questions.js
@@ -0,0 +1,107 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of these file types is not an adobe file type?",
+    answer: "Vector",
+    options: ["Vector", "Illustrator", "Framemaker", "Trick Question!"],
+  },
+  {
+    numb: 2,
+    question: "Which of these is not a JPEG file extension?",
+    answer: ".JGI",
+    options: [".JNG", ".JXL", ".JGI", "Trick Question!"],
+  },
+  {
+    numb: 3,
+    question: "Which of these is not a type of spreadsheet file?",
+    answer: "SumCalc",
+    options: ["SuperCalc", "SumCalc", "StarCalc", "Trick Question!"],
+  },
+  {
+    numb: 4,
+    question: "Which of these is not an EXIF file extension?",
+    answer: ".GIF",
+    options: [".WAV", ".JPEG", ".GIF", "Trick Question!"],
+  },
+  {
+    numb: 5,
+    question: "Which of these is not a Microsoft file extension?",
+    answer: ".XLZ",
+    options: [".XLA", ".XLZ", ".XLK", "Trick Question!"],
+  },
+  {
+    numb: 6,
+    question: "Which of these is not an audio focused file?",
+    answer: "Flacwave",
+    options: ["Flacwave", "Waveform", "BroadcastWave", "Trick Question!"],
+  },
+  {
+    numb: 7,
+    question: "Which of these is not an email file type?",
+    answer: "EBOX",
+    options: ["Outlook Express Message", "MBOX", "EBOX", "Trick Question!"],
+  },
+  {
+    numb: 8,
+    question: "Which of these is not a text based file extension?",
+    answer: ".XYZ",
+    options: [".OTT", ".WP4X", ".XYZ", "Trick Question!"],
+  },
+  {
+    numb: 9,
+    question: "Which of these is not an Apple developed file type?",
+    answer: "iaudio",
+    options: ["iaudio", "ibook", "iwork", "Trick Question!"],
+  },
+  {
+    numb: 10,
+    question: "Which of these is not a PDF file extension?",
+    answer: "Trick Question!",
+    options: ["PDF/A", "PDF/R", "PDF/X", "Trick Question!"],
+  },
+  {
+    numb: 11,
+    question: "Which of these is not a 3D file format?",
+    answer: "BodyForm3D",
+    options: ["Blitz3D", "BodyPaint3D", "BodyForm3D", "Trick Question!"],
+  },
+  {
+    numb: 12,
+    question: "Which of these is not a Zip file format?",
+    answer: "Trick Question!",
+    options: ["GZip", "7Zip", "Bzip", "Trick Question!"],
+  },
+  {
+    numb: 13,
+    question: "Which of these is not an adobe file format?",
+    answer: "Freehand",
+    options: ["InDesign", "Freehand", "Flash", "Trick Question!"],
+  },
+  {
+    numb: 14,
+    question: "Which of these is not an archive file format?",
+    answer: "DROID",
+    options: ["Stuffit", "WARC", "DROID", "Trick Question!"],
+  },
+  {
+    numb: 15,
+    question: "Which of these is not a database file format?",
+    answer: "FoxLite",
+    options: ["FoxPro", "FoxLite", "FileMaker Pro", "Trick Question!"],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
